3903588631ca6bdcfbb456b35a6600a1ac55c29a
preston <broberts9@gmail.com>
2018-02-05 20:59:49 -0800

    troubleshooting heroku crash

--- config.js
+++ config.js
@@ -2,9 +2,6 @@ exports.DATABASE_URL =
     process.env.DATABASE_URL || 'mongodb://localhost/peach-of-mind';
 exports.TEST_DATABASE_URL =
     process.env.DATABASE_URL || 'mongodb://localhost/peach-of-mind-test';
-exports.PORT = 8080;
+exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'idontknow';
-exports.JWT_EXPIRY = '7d';
-
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-// exports.PORT = process.env.PORT || 8080;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
