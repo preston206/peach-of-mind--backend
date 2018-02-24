0e874d01f0a5831c7c883ac04cf6c73cdd46f237
preston <broberts9@gmail.com>
2018-02-23 19:23:20 -0800

    troubleshooting safari login

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://peachofmind.netlify.com');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin,Accept,Content-Type,Authorization,Content-Length,X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     if (req.method === 'OPTIONS') {
         return res.send(204);
     }
