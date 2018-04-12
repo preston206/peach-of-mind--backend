91df860a790775c5bd948f0200c8f1721bd1b4b5
preston <broberts9@gmail.com>
2018-04-11 21:41:08 -0700

    updating config with new database variables

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
-exports.DATABASE_URL =
-    process.env.DATABASE_URL || 'mongodb://localhost/peach-of-mind';
-exports.TEST_DATABASE_URL =
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost/peach-of-mind-test';
+exports.PROD_DATABASE_MLAB_URL =
+    process.env.PROD_DATABASE_MLAB_URL || 'mongodb://localhost/peach-of-mind-production';
+exports.TEST_DATABASE_LOCALHOST_URL =
+    process.env.TEST_DATABASE_LOCALHOST_URL || 'mongodb://localhost/peach-of-mind-test';
+exports.TEST_DATABASE_MLAB_URL =
+    process.env.TEST_DATABASE_MLAB_URL || 'mongodb://localhost/peach-of-mind-test';
 // !Important! process.env.PORT is needed for deploying to Heroku
 // the build will crash, on Heroku side, if the port is hard coded
 // heroku configures it with a dynamic port at build time
